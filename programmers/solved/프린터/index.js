4e335747585933a6cccd8ea4a35aa73ed8bd6505
cckn <cckn1985@gmail.com>
2019-08-28 01:53:43 +0900

    Add "프린터" solved

--- programmers/solved/프린터/index.js
+++ programmers/solved/프린터/index.js
@@ -4,19 +4,17 @@ function solution(priorities, location) {
         idx === location
     ]);
 
-    let done = false;
     let count = 0;
-    while (!done) {
+    while (pages) {
         const [currentPage, isMine] = pages.shift();
 
         if (pages.some(([page]) => currentPage < page)) {
             pages.push([currentPage, isMine]);
         } else {
             count++;
-            done = isMine;
+            if (isMine) return count;
         }
     }
-    return count;
 }
 
 const results = [solution([2, 1, 3, 2], 2), solution([1, 1, 9, 1, 1, 1], 0)];
