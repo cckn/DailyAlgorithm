ed0790a80a7befce92902fed1a2cb658c044196d
cckn <pds@enitt.co.kr>
2020-12-08 16:38:56 +0900

    utils.js 배열 비교 부분 추가

--- algorithm/src/index.js
+++ algorithm/src/index.js
@@ -1,30 +1,6 @@
-function solution(board, moves) {
-  const _board = Array(board.length)
-    .fill(null)
-    .map(() => Array())
-
-  const stack = Array()
-  let result = 0
-
-  board.reverse().forEach((line) => {
-    line.forEach((element, idx) => {
-      if (element !== 0) {
-        _board[idx].push(element)
-      }
-    })
-  })
-
-  moves.forEach((x) => {
-    const pop = _board[x - 1].pop()
-    if (pop) {
-      if (pop === stack[stack.length - 1]) {
-        stack.pop()
-        result += 2
-      } else stack.push(pop)
-    }
-  })
-
-  return result
+function solution(numbers) {
+  var answer = [2, 3, 4, 5, 6, 7]
+  return answer
 }
 
 //////////////////////////
@@ -33,17 +9,12 @@ const { algoTest } = require('./utils')
 
 const testFunc = solution
 const params = [
-  [
-    [
-      [0, 0, 0, 0, 0],
-      [0, 0, 1, 0, 3],
-      [0, 2, 5, 0, 1],
-      [4, 2, 4, 4, 2],
-      [3, 5, 1, 3, 1],
-    ],
-    [1, 5, 3, 5, 1, 2, 1, 4],
-  ],
+  [2, 1, 3, 4, 1],
+  [5, 0, 2, 7],
 ]
-const expects = [4]
+const expects = [
+  [2, 3, 4, 5, 6, 7],
+  [2, 5, 7, 9, 12],
+]
 
 algoTest(testFunc, params, expects)
--- algorithm/src/utils.js
+++ algorithm/src/utils.js
@@ -1,15 +1,35 @@
 const algoTest = (testFunction, params, expects) => {
   params.forEach((param, idx) => {
     const result = testFunction(...param)
+    const expect = expects[idx]
+
+    let isMatch = result === expect
+    if (Array.isArray(expect)) {
+      isMatch = arrayMatch(result, expect)
+    }
+
     const resultMsg = `
     result -> ${result} 
-    expect -> ${expects[idx]}
-    \t\t\t${result === expects[idx] ? '' : 'plz check this param'}
+    expect -> ${expect}
+    \t\t\t${isMatch ? '' : 'plz check this param'}
     ----------------
     `
     console.log('\n', param)
     console.log(resultMsg)
   })
+}
+
+const arrayMatch = (arr1, arr2) => {
+  // Check if the arrays are the same length
+  if (arr1.length !== arr2.length) return false
+
+  // Check if all items exist and are in the same order
+  for (var i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false
+  }
+
+  // Otherwise, return true
+  return true
 }
 
 module.exports = { algoTest }
