99cefa698fb5a992455815fa9906531293c35d23
cckn <pds@enitt.co.kr>
2021-04-05 10:09:09 +0900

    update how to create two dimensional array

--- cheatsheet/js/array.js
+++ cheatsheet/js/array.js
@@ -1,7 +1,12 @@
-// 길이가 5인 빈 배열 선언
 const n = 5
 const m = 10
-console.log(new Array(n).fill(null))
 
-// n*m 빈 배열 선언
-console.log(new Array(n).fill(new Array(m)))
+// 길이가 5인 빈 배열 선언
+console.log(new Array(n).fill(0))
+
+// n * m 빈 배열 선언
+const matrix1 = Array.from(Array(n), () => new Array(m).fill(1))
+const matrix2 = new Array(n).fill(null).map(() => new Array(m).fill(2))
+
+console.log(matrix1)
+console.log(matrix2)
